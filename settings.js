aa8ad58bfeac92e1e81f03d0f996c0c90836f1b2
deeikg <115475928+deeikg@users.noreply.github.com>
2022-10-14 15:29:00 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -12,11 +12,11 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['94784506970']
-global.premium = ['94784506970']
-global.ownernomer = '94784506970'
-global.ownername = 'Achi Fernando'
-global.botname = '🎭ᴀʟᴘʜᴀ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ-ᴍᴅ ᴇᴅɪᴛɪᴏɴ🎭 v1.0.0'
+global.owner = ['940725243969']
+global.premium = ['940725243969']
+global.ownernomer = '940725243969'
+global.ownername = 'RON MD'
+global.botname = '🎭MR TON⚡🎭 v1.0.0'
 global.footer = ' ᴀʟᴘʜᴀ ʙᴏᴛ-ᴍᴅ ᴇᴅɪᴛɪᴏɴ'
 global.ig = 'https://github.com/ALPHA-OFFICIAL-TEAM'
 global.region = 'India, South India, Kerala'
